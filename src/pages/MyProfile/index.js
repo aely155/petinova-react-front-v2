687485858aa9cbdf32e2d7d273ab9a828ccd476c
aely155 <aely155@gmail.com>
2024-07-05 20:24:47 -0300

    first commit

--- src/pages/MyProfile/index.js
+++ src/pages/MyProfile/index.js
@@ -1,12 +1,18 @@
 import Cart from '../../components/Cart';
-import DesconectButton from '../../components/DesconectButton';
+import OptionButton from '../../components/OptionButton';
 import Header from '../../components/Header';
 import LateralButtons from '../../components/LateralButtons';
 import Main from '../../components/Main';
 import ProfileCard from '../../components/ProfileCard';
 import './style.css'
+import { IoMdExit } from 'react-icons/io';
+import { FiPackage } from 'react-icons/fi';
 
 function MyProfile() {
+    const desconect = () => {
+        localStorage.clear()
+        window.location.reload()
+    }
     return (
         <>
             <Cart />
@@ -14,7 +20,14 @@ function MyProfile() {
             <Header />
             <Main>
                 <ProfileCard />
-                <DesconectButton/>
+                <OptionButton bg="gray" color='white'>
+                    <FiPackage style={{ marginRight: "10px" }} />
+                    Meus Pedidos
+                </OptionButton>
+                <OptionButton onclick={desconect} bg="red">
+                    <IoMdExit style={{ marginRight: "10px" }} />
+                    Sair
+                </OptionButton>
             </Main>
         </>
     );
