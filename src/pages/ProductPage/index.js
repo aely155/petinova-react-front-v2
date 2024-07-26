159cf2932f18c55887531decbd74445919a361e3
aely155 <aely155@gmail.com>
2024-07-26 02:47:25 -0300

    first commit

--- src/pages/ProductPage/index.js
+++ src/pages/ProductPage/index.js
@@ -46,36 +46,39 @@ function ProductPage() {
             <Header />
             <LateralButtons />
             <Main>
-
-                <div className="product-page">
-                    <img src={product.urlimage} />
-                    <div>
-                        <h2>{product.name}</h2>
-                        <h4>Por</h4>
-                        <h1>R${product.price.replace(".", ",")}</h1>
-                    </div>
-                    <div className="product-page-buttons">
-                        <div className="product-page-buttons">
-                            <button>Adicionar ao carrinho</button>
-                            <button>Comprar agora</button>
+                {product &&
+                    <>
+                        <div className="product-page">
+                            <img src={product.urlimage} />
+                            <div>
+                                <h2>{product.name}</h2>
+                                <h4>Por</h4>
+                                <h1>R${product.price.replace(".", ",")}</h1>
+                            </div>
+                            <div className="product-page-buttons">
+                                <div className="product-page-buttons">
+                                    <button>Adicionar ao carrinho</button>
+                                    <button>Comprar agora</button>
+                                </div>
+                            </div>
+                            <div className="product-page-shiping">
+                                <h3>Receba em sua casa em até 5 dias, por nossa conta!</h3>
+                                <TbTruckDelivery color="#458DBB" size={40}
+                                />
+                            </div>
+                            <div className="product-page-description">
+                                {description.map(description => {
+                                    return (
+                                        <p>
+                                            {description}
+                                        </p>
+                                    )
+                                })}
+                            </div>
+                            <iframe style={{ borderRadius: "25px", margin: "20px 0px" }} width="100%" height="215" src="https://www.youtube.com/embed/Icdew6aFnFU?si=OlVaPHbgO_h4BKBy&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                         </div>
-                    </div>
-                    <div className="product-page-shiping">
-                        <h3>Receba em sua casa em até 5 dias, por nossa conta!</h3>
-                        <TbTruckDelivery color="#458DBB" size={40}
-                        />
-                    </div>
-                    <div className="product-page-description">
-                        {description.map(description => {
-                            return (
-                                <p>
-                                    {description}
-                                </p>
-                            )
-                        })}
-                    </div>
-                    <iframe style={{ borderRadius: "25px", margin: "20px 0px" }} width="100%" height="215" src="https://www.youtube.com/embed/Icdew6aFnFU?si=OlVaPHbgO_h4BKBy&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                </div>
+                    </>
+                }
             </Main>
         </>
     );
