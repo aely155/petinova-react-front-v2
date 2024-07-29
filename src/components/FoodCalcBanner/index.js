ac976ca71e1417f64094c58cd78dc082530eda54
aely155 <aely155@gmail.com>
2024-07-29 04:34:46 -0300

    first commit

--- src/components/FoodCalcBanner/index.js
+++ src/components/FoodCalcBanner/index.js
@@ -1,16 +1,19 @@
 import './style.css'
 import cat from './cat.png'
+import { Link } from 'react-router-dom';
 function FoodCalcBanner() {
     return (
         <div className='foodcalcbanner'>
-            <img src={cat} alt='calculadora de ração gato segurnado pote de ração'/>
+            <img src={cat} alt='calculadora de ração gato segurnado pote de ração' />
             <div className='foodcalcbanner-content'>
-                <h1>Descubra a Porção Ideal de Alimento para Seu Cão ou Gato: <br/>Garanta a Saúde e Felicidade do Seu Pet</h1>
-                
-            <div className='foodcalcbanner-content-box'>
-                <p>Encontrar a quantidade certa de alimento para seu cão ou gato é essencial para manter a saúde e a felicidade do seu pet. A porção ideal depende de vários fatores, como idade, peso e nível de atividade.</p>
-                <button>Quero descobrir</button>
-            </div>
+                <h1>Descubra a Porção Ideal de Alimento para Seu Cão ou Gato: <br />Garanta a Saúde e Felicidade do Seu Pet</h1>
+
+                <div className='foodcalcbanner-content-box'>
+                    <p>Encontrar a quantidade certa de alimento para seu cão ou gato é essencial para manter a saúde e a felicidade do seu pet. A porção ideal depende de vários fatores, como idade, peso e nível de atividade.</p>
+                    <Link to={'/pet-food'}>
+                        <button>Quero descobrir</button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
