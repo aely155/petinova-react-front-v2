9ebb2003b98970d7adc3fca46ba4d347e3631eac
aely155 <aely155@gmail.com>
2024-08-14 18:35:10 -0300

    first commit

--- src/pages/ProductPage/index.js
+++ src/pages/ProductPage/index.js
@@ -21,13 +21,14 @@ function ProductPage() {
     const [description, setDescription] = useState([])
 
     const [previousPage, setPreviousPage] = useState()
-
-    // Extrai o ID do vídeo do YouTube do link completo
+    
     const getYouTubeId = (url) => {
+        if (!url) return null; // Adicione essa verificação
         const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
         const match = url.match(regExp);
         return (match && match[2].length === 11) ? match[2] : null;
     };
+    
 
     const videoId = getYouTubeId(product.urlvideo);
 
